23b9fa274083e72a5669f53a692eb2b5c3321a61
Бояринцев Александр <a.bo@bramblingtech.com>
2021-11-24 13:02:17 +0200

    fix create.sql, select.sql

--- create.sql
+++ create.sql
@@ -1,52 +1,160 @@
-CREATE TABLE `films` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- PostgreSQL database dump
+--
 
-CREATE TABLE `halls` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- Dumped from database version 10.12
+-- Dumped by pg_dump version 10.12
 
-CREATE TABLE `sessions` (
-  `id` int(11) NOT NULL,
-  `hall_id` int(11) NOT NULL,
-  `film_id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `price` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
-CREATE TABLE `tickets` (
-  `id` int(11) NOT NULL,
-  `session_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
 
-
-ALTER TABLE `films`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
-ALTER TABLE `halls`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `hall_id` (`hall_id`),
-  ADD KEY `film_id` (`film_id`);
-
-ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `session_id` (`session_id`);
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
-ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
 
-ALTER TABLE `halls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
-ALTER TABLE `sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: films; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.films (
+    id bigint NOT NULL,
+    name character varying(128)
+);
+
+
+ALTER TABLE public.films OWNER TO postgres;
+
+--
+-- Name: films_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.films_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.films_id_seq OWNER TO postgres;
+
+--
+-- Name: films_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.films_id_seq OWNED BY public.films.id;
+
+
+--
+-- Name: halls; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.halls (
+    id integer NOT NULL,
+    name character varying(64) NOT NULL
+);
+
+
+ALTER TABLE public.halls OWNER TO postgres;
+
+--
+-- Name: prices; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.prices (
+    id integer NOT NULL,
+    price numeric(4,2),
+    name character varying(128)
+);
+
+
+ALTER TABLE public.prices OWNER TO postgres;
+
+--
+-- Name: films id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.films ALTER COLUMN id SET DEFAULT nextval('public.films_id_seq'::regclass);
+
+
+--
+-- Data for Name: films; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.films (id, name) FROM stdin;
+\.
+
+
+--
+-- Data for Name: halls; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.halls (id, name) FROM stdin;
+\.
+
+
+--
+-- Data for Name: prices; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.prices (id, price, name) FROM stdin;
+\.
+
+
+--
+-- Name: films_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.films_id_seq', 1, false);
+
+
+--
+-- Name: films films_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.films
+    ADD CONSTRAINT films_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: halls halls_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.halls
+    ADD CONSTRAINT halls_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: prices prices_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.prices
+    ADD CONSTRAINT prices_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
